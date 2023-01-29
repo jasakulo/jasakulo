53f9b90c9f08de6abf80244950a06d2c4bcd833a
jasakulo <jasakuloapp@gmail.com>
2023-01-30 02:49:28 +0700

    update animasi slidbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
       center: true,
       padding: '16px',
     },
-    extend: {
-      colors:{
+    extend: {    
+     colors:{
         primary:'#14b8a6',
         secondary:'#64748b',
         dark:'#0f172a',
