c3fe9d8c5609d72c52629d4846bfb19b34182e9b
jasakulo <jasakuloapp@gmail.com>
2023-02-13 14:35:34 +0700

    update logo terbaru

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
-    extend: {    
-     colors:{
-        primary:'#14b8a6',
-        secondary:'#64748b',
-        dark:'#0f172a',
-        green:'#00FFFF',
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        green: "#00FFFF",
       },
-      screens:{
-        '2xl':'1320px',
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
+};
